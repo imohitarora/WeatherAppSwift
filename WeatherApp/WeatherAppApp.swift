4e4ec70d301904b1c8abcb65e8bb95bf4c1a8a72
Mohit Arora <me@aboutmohit.com>
2024-04-17 17:48:20 -0400

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Mohit Arora on 2024-04-17.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
